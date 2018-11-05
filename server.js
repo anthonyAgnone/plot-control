e337d2ba1f017eba3f5bbcd6df5c4bc5d521a8d7
Anthony Agnone <ada60@pitt.edu>
2018-11-05 11:08:48 -0700

    debugging herokue deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const axios = require('axios');
 const passport = require('passport');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
